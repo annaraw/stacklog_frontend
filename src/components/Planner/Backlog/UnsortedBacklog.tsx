cb07bd8ae954c423d3c06fadaf9df2349e1e8aa7
THOMAS-PC\Thomas <thomas.sedlmayer@tum.de>
2020-07-14 21:50:19 +0200

    Added button for adding backlog item

--- src/components/Planner/Backlog/UnsortedBacklog.tsx
+++ src/components/Planner/Backlog/UnsortedBacklog.tsx
@@ -1,9 +1,14 @@
+
 import * as React from 'react';
 import { IBacklogItem, Column } from '../../../models/models';
 import BacklogItem from './BacklogItem';
 import { Droppable } from 'react-beautiful-dnd'
 import styled from 'styled-components'
 import Scrollbar from 'react-scrollbars-custom';
+import { IconButton, Tooltip } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+
+import { unsortedBacklogStyles } from './UnsortedBacklogStyles';
 
 interface BoardColumnProps {
   column: Column,
@@ -25,38 +30,46 @@ const BoardColumnContent = styled.div<BoardColumnContentStylesProps>`
 `
 
 export const UnsortedBacklog: React.FC<BoardColumnProps> = (props) => {
-    return (
-      <React.Fragment>
-      <div className="backlogContainer">
-        <div className="containerTitle">{props.title}</div>
-        <Scrollbar style={{ width: "100%", float: "left", height: "500px" }}>
-        <Droppable droppableId={props.column.id}>
-          {(provided, snapshot) => (
-            <BoardColumnContent
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              isDraggingOver={snapshot.isDraggingOver}
-            >
-              {props.items.map((backlogItem: IBacklogItem, index: number) => 
-                  
-                    <BacklogItem 
-                      index={index} 
-                      id={backlogItem.id} 
-                      key={backlogItem.id} 
-                      title={backlogItem.title} 
-                      description={backlogItem.description ? backlogItem.description : ""}
-                      category={backlogItem.category} 
-                      priority={backlogItem.priority}
-                    />
-                    
+  const classes = unsortedBacklogStyles()
+  return (
+    <React.Fragment>
+      <div className={classes.backlogContainer}>
+        <Tooltip title="Add new Backlog Item" placement="left" arrow>
+          <IconButton className={classes.addButton} aria-label="add">
+            <AddIcon />
+          </IconButton>
+        </Tooltip>
+        <div className={classes.containerTitle}>{props.title}</div>
+        <Scrollbar className={classes.scrollbar}>
+          <Droppable droppableId={props.column.id}>
+            {(provided, snapshot) => (
+              <BoardColumnContent
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                isDraggingOver={snapshot.isDraggingOver}
+              >
+                {props.items.map((backlogItem: IBacklogItem, index: number) =>
+
+                  <BacklogItem
+                    index={index}
+                    id={backlogItem.id}
+                    key={backlogItem.id}
+                    title={backlogItem.title}
+                    description={backlogItem.description ? backlogItem.description : ""}
+                    category={backlogItem.category}
+                    priority={backlogItem.priority}
+                  />
+
                 )}
-              {provided.placeholder}
-            </BoardColumnContent>
-          )}
-        </Droppable>
+                {provided.placeholder}
+              </BoardColumnContent>
+            )}
+          </Droppable>
+
         </Scrollbar>
+
       </div>
-      </React.Fragment>
-    );
+    </React.Fragment>
+  );
 
 };
