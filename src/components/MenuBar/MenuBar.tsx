a855d15e4490af6535231ed2a6fbd71f20f4235e
annarama <anna.rauscher@tum.de>
2020-07-17 17:19:19 +0200

    permanent tiny menu bar

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import clsx from 'clsx';
 import { useState } from 'react'
 import {
     AppBar, Button, Toolbar, Drawer, Box, Tooltip,
-    IconButton, Typography
+    IconButton, Typography, CssBaseline
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import Logo from '../LandingPage/Images/Logo.png';
@@ -12,6 +13,7 @@ import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
 import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
 import { menuBarStyles } from './MenuBarStyles';
 import UserService from '../../services/UserService';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 interface MenuBarProps {
     title: string,
@@ -30,33 +32,62 @@ function MenuBar(props: MenuBarProps) {
         setIsopen(true)
     )
     return (
-        <React.Fragment>
-            <AppBar position="fixed">
+        <div className = {classes.root}>
+            <AppBar 
+            position="fixed"
+            className={clsx(classes.appBar,
+                {[classes.appBarShift]: isOpen,
+            })}>
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"
-                        onClick={openPanel} disabled={props.disableButton}>
+                    <IconButton 
+                    edge="start" 
+                    color="inherit" 
+                    aria-label="menu"
+                    onClick={openPanel} disabled={props.disableButton}>
                         <MenuIcon />
                     </IconButton>
-                    <IconButton edge="start" color="inherit" aria-label="menu" size='medium'
-                        onClick={() => { window.location.href = "/" }}>
+
+                    <IconButton 
+                    className={classes.menuIcon}
+                    edge="start" 
+                    color="inherit" 
+                    aria-label="menu" 
+                    size='medium'
+                    onClick={() => { window.location.href = "/" }}>
                         <img src="./assets/logo_white.svg"  className={classes.image} />
                     </IconButton>
+
                     <Button variant =  "outlined" className={classes.loginBtn}
                         onClick={() => { !UserService.getCurrentUser().id ? window.location.href = "/login" : UserService.logout() }}>
                         {UserService.getCurrentUser().id ? <span>Logout</span> : <span>Login</span>}
                     </Button>
                 </Toolbar>
             </AppBar>
-            <div className={classes.drawerDiv}>
+            <div >
                 <Drawer
-                    anchor="left"
+                    
+                    variant="permanent"
                     open={isOpen}
                     onClose={dismissPanel}
+                    className={clsx(classes.drawer,{
+                        [classes.drawerOpen]:isOpen, 
+                        [classes.drawerClose]: !isOpen})}
+                    classes={{
+                        paper: clsx({
+                            [classes.drawerOpen]: isOpen,
+                            [classes.drawerClose]: !isOpen,
+                        }),
+                    }}
                 >
+                    <div className={classes.toolbar}>
+                        <IconButton onClick={dismissPanel} >
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </div>
                     <Box >
-                        <p className={classes.menuTitle}><strong>Menu</strong></p>
-                        <div className={classes.menuOption}>
-                            <Button onClick={() => { window.location.href = "/home" }}>
+                        
+                        <div  className={classes.menuOption}>
+                            <Button onClick={() => { window.location.href = "/home" }} >
                                 <HomeIcon className={classes.menuOption} />
                             Home
                             </Button>
@@ -82,7 +113,7 @@ function MenuBar(props: MenuBarProps) {
                     </Box>
                 </Drawer>
             </div>
-        </React.Fragment >
+        </div>
 
     )
 }
--- src/components/MenuBar/MenuBarStyles.ts
+++ src/components/MenuBar/MenuBarStyles.ts
@@ -1,17 +1,11 @@
 import { makeStyles } from "@material-ui/core";
 
+const drawerWidth = 200;
+
 export const menuBarStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-    drawerDiv: {
-        width: "400px",
+        display: 'flex',
     },
     image: {
         width: '120px',
@@ -19,29 +13,73 @@ export const menuBarStyles = makeStyles((theme) => ({
         marginTop:"4px"
     },
     menuOption: {
+        width: "drawerWidth",
         marginTop: "10px",
         marginBottom: "10px",
-        marginLeft: "2px",
-        marginRight: "10px",
+        marginLeft: "6px",
+        marginRight: "20px",
     },
     menuIcon: {
-        marginTop: "10px",
-        marginBottom: "10px",
-        marginLeft: "2px",
-        marginRight: "10px",
-    },
-    menuIcons: {
         marginLeft: "10px"
-    },
-    menuTitle: {
-        marginTop: "10px",
-        marginBottom: "10px",
-        marginLeft: "10px",
     },
     loginBtn: {
         position: "absolute",
         right: "20px",
         color: "white",
         borderColor: "white "
-  }
-}));
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerClose: {
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      overflowX: 'hidden',
+      width: theme.spacing(7) + 1,
+      [theme.breakpoints.up('sm')]: {
+        width: theme.spacing(8) + 1,
+      },
+    },
+    toolbar: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  }),
+);
